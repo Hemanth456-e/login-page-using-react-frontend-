9621b0f552f095697f46d73d7156225f7133d745
HEMANTH <majjihemanth1105@gmail.com>
2026-07-26 03:10:35 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,49 @@
+import { useState } from "react";
+import API from "../services/api";
 import "./Login.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const response = await API.post("/login", {
+        email,
+        password,
+      });
+
+      alert(response.data.message);
+    } catch (error) {
+      alert(error.response?.data?.message || "Login Failed");
+    }
+  };
+
   return (
     <div className="container">
       <div className="login-box">
         <h2>Login</h2>
 
         <label>Email</label>
-        <input type="email" placeholder="Enter your email" />
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
         <label>Password</label>
-        <input type="password" placeholder="Enter your password" />
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <button>Login</button>
+        <button onClick={handleLogin}>Login</button>
 
         <p>
-          Don't have an account? <a href="#">Sign Up</a>
+          Don't have an account? <a href="#">Register</a>
         </p>
       </div>
     </div>
